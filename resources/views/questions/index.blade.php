ae159c4c4fb2bbce4a13e4e29cd213f5d42f0fde
tlae <phile2002tz@yahoo.com>
2019-01-12 20:46:01 +0300

    Add answers and views counter to question item

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -8,25 +8,40 @@
                 <div class="card-header">Questions</div>
 
                 <div class="card-body">
-                   @foreach ($questions as $question)
-                     <div class="media">
-                      <div class="media-body">
+                       @foreach ($questions as $question)
+                          <div class="media">
 
-                       <h3 class="mt-0"><a href="{{$question->url}}">{{ $question->title}}</a></h3>
-                        <p class="lead">
-                          Asked by
-                          <a href="{{$question->user->url}}">{{$question->user->name}}</a>
-                          <small class="text-muted">{{$question->created_date}}</small>
-                        </p>
+                          <div class="d-flex flex-column counters">
+                           <div class="vote">
+                             <strong> {{$question->votes}}</strong> {{str_plural('vote',$question->votes)}}
+                           </div>
+                           <div class="status {{$question->status}}">
+                             <strong> {{$question->answers}}</strong> {{str_plural('answer',$question->answers)}}
+                           </div>
+                           <div class="view">
+                            {{$question->views . " " . str_plural('view',$question->views)}}
+                           </div>
+                          </div>
+                        
+                           <div class="media-body">
 
-                       {{str_limit($question->body, 250)}}
+                           <h3 class="mt-0"><a href="{{$question->url}}">{{ $question->title}}</a></h3>
+                           <p class="lead">
+                             Asked by
+                             <a href="{{$question->user->url}}">{{$question->user->name}}</a>
+                             <small class="text-muted">{{$question->created_date}}</small>
+                           </p>
+
+                          {{str_limit($question->body, 250)}}
+                         </div>
+                    
                       </div>
-                     </div>
-                     <hr>
-                   @endforeach
-                   <div class="pagination justify-content-center pagination-lg">
-                     {{$questions->links()}}
-                   </div>
+                      <hr>
+                     @endforeach
+
+                      <div class="pagination justify-content-center pagination-lg">
+                        {{$questions->links()}}
+                      </div>
                 </div>
             </div>
         </div>
