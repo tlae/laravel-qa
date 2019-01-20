1ae784d0d2ad12fdb42c13bbddd0bc532edd33e7
tlae <phile2002tz@yahoo.com>
2019-01-20 17:30:53 +0300

    implement delete functionality for the question

--- app/Question.php
+++ app/Question.php
@@ -18,7 +18,7 @@ class Question extends Model
  	}
 
  	public function getUrlAttribute() {
- 		return route("questions.show", $this->id);
+ 		return route("questions.show", $this->slug);
  		return "#";
  	}
 
@@ -34,6 +34,9 @@ class Question extends Model
  			return "answered";
  		}
  		return "unanswered";
+ 	}
+ 	public function getBodyHtmlAttribute() {
+ 		return \Parsedown::instance()->text($this->body);
  	}
 
 }
