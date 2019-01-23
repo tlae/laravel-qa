255a6967dbca7980974fe2175a2c77d3c3ac6039
tlae <phile2002tz@yahoo.com>
2019-01-23 18:13:56 +0300

    Design answers table schema and rename answers column in the question table

--- app/Question.php
+++ app/Question.php
@@ -11,6 +11,9 @@ class Question extends Model
 	public function user() {
 		return $this->belongsTo(User::class);
 	}
+	public function answers() {
+ 		return $this->hasMany(Answer::class);
+ 	}
 	public function setTitleAttribute($value)
 	{
       $this->attributes['title'] = $value;
@@ -27,7 +30,7 @@ class Question extends Model
  	}
 
  	public function getStatusAttribute() {
- 		if ($this->answers > 0) {
+ 		if ($this->answers_count > 0) {
  			if ($this-> best_answer_id) {
  				return "answered-accepted";
  			}
--- app/User.php
+++ app/User.php
@@ -32,10 +32,13 @@ class User extends Authenticatable
     {
 	return $this->hasMany(Question::class);
     }
-
+    public function answers() {
+        return $this->hasMany(Answer::class);
+    }
     public function getUrlAttribute() {
        // return route("question.show", $this->id);
         return "#";
     }
+
 
 }
